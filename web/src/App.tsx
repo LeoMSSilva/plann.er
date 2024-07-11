c6e378438138017ebfe6aadf9c7620006c9ac94c
LeoMSSilva <l.eom1994@hotmail.com>
2024-07-11 09:33:31 -0300

    chore(web): customized page not found

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Pattern from "./assets/pattern.png";
 import { TripCreation } from "./pages/trip-creation";
 import { TripDetails } from "./pages/trip-details";
 
@@ -14,9 +16,17 @@ const router = createBrowserRouter([
   {
     path: "*",
     element: (
-      <h1 className="text-4xl font-bold text-zinc-300">
-        Página não encontrada!
-      </h1>
+      <div className="h-screen flex justify-center items-center">
+        <div className="absolute">
+          <img
+            src={Pattern}
+            alt="Imagem de fundo"
+          />
+        </div>
+        <h1 className="text-4xl font-bold text-zinc-300">
+          Página não encontrada!
+        </h1>
+      </div>
     ),
   },
 ]);
