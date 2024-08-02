8f97ae349579ba8ccedcecfa1ab08e9608054032
LeoMSSilva <l.eom1994@hotmail.com>
2024-08-01 22:51:29 -0300

    feature(server): created an email layout to notify travel information updates with the option to confirm participation

--- server/src/templates/index.ts
+++ server/src/templates/index.ts
@@ -1,4 +1,5 @@
 import { mailConfirmTrip } from "./mail-confirm-trip";
+import { mailConfirmUpdateTrip } from "./mail-confirm-trip-update";
 import { mailCreateTrip } from "./mail-create-trip";
 
-export { mailConfirmTrip, mailCreateTrip };
+export { mailConfirmTrip, mailConfirmUpdateTrip, mailCreateTrip };
---
+++ server/src/templates/mail-confirm-trip-update.ts
@@ -0,0 +1,57 @@
+interface ITemplate {
+  participantName: string;
+  destination: string;
+  formattedStartDate: string;
+  formattedEndDate: string;
+  confirmationLink: string;
+  isConfirmed: boolean;
+}
+
+export const mailConfirmUpdateTrip = ({
+  participantName,
+  destination,
+  formattedStartDate,
+  formattedEndDate,
+  confirmationLink,
+  isConfirmed,
+}: ITemplate) =>
+  `
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body style="height: 100vh; width: 100%; margin: 0; padding: 0; text-align: center; font-family: sans-serif; font-size: 16px; line-height: 1.6; background-color: #09090b; color: #52525b; ">
+    <div style="display: inline-block; width: 64%; margin: 100px auto">
+      <h1>Olá,
+        <strong style="color: #a1a1aa">${participantName}!</strong>
+      </h1>
+      <p style="font-size: 18px">A viagem que você foi convidado(a) foi atualizada.</p>
+      <br style="font-size: 18px">Com destino para
+        <strong style="color: #a1a1aa">${destination}</strong> nas datas de
+        <strong style="color: #a1a1aa">${formattedStartDate}</strong> até
+        <strong style="color: #a1a1aa">${formattedEndDate}</strong>.
+      </p>
+      ${
+        !isConfirmed &&
+        ` <p style="font-size: 18px">
+        Para confirmar sua presença, clique no link abaixo:
+        </p>
+        <br>
+        <p>
+        <a style="color: #1a2e05; text-decoration: none; background-color: #bef264; padding: 16px 32px; border-radius: 8px" href=${confirmationLink}>
+        Confirmar presença
+        </a>
+        </p>`
+      }
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <p style="font-size: 14px">
+        Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.
+      </p>
+    </div>
+  </body>
+</html>
+`.trim();
